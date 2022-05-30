c5a09380617c8db3e59e4015cfc8c3239002540c
Clément Gorin <gorinclem@gmail.com>
2022-05-30 11:24:32 +0200

    Update 2022.05.30

--- arthisto1960_statistics.py
+++ arthisto1960_statistics.py
@@ -4,7 +4,7 @@
 @description: Computes statistics statistics for the Arthisto 1960 project
 @author: Clement Gorin
 @contact: gorinclem@gmail.com
-@version: 2022.03.23
+@version: 2022.05.30
 '''
 
 #%% HEADER
@@ -26,28 +26,26 @@ cities      = dict(paris='0625_6870|0650_6870', marseille='0875_6245|0875_6270',
 
 #%% FUNCTIONS
 
-# Computes prediction sets
 def compute_sets(label_test:np.ndarray, label_pred:np.ndarray) -> np.ndarray:
-    # Formats labels
+    '''Computes prediction sets'''
     label_test = label_test.astype(bool)
     label_pred = label_pred.astype(bool)
-    # Computes masks
-    pixset_tp = np.logical_and(label_test, label_pred)
-    pixset_tn = np.logical_and(np.invert(label_test), np.invert(label_pred))
-    pixset_fp = np.logical_and(np.invert(label_test), label_pred)
-    pixset_fn = np.logical_and(label_test, np.invert(label_pred))
-    pixsets   = np.array([pixset_tp, pixset_tn, pixset_fp, pixset_fn])
-    return pixsets
+    set_tp = np.logical_and(label_test, label_pred)
+    set_tn = np.logical_and(np.invert(label_test), np.invert(label_pred))
+    set_fp = np.logical_and(np.invert(label_test), label_pred)
+    set_fn = np.logical_and(label_test, np.invert(label_pred))
+    sets   = np.array([set_tp, set_tn, set_fp, set_fn])
+    return sets
 
-# Computes subset without borders
 def mask_borders(sets:np.ndarray, label_test:np.ndarray) -> np.ndarray:
+    '''Computes subset without borders'''
     subset = np.invert(segmentation.find_boundaries(label_test))
     subset = np.tile(subset, (sets.shape[0], 1, 1, 1))
     masked = np.where(subset, sets, False)
     return masked 
 
-# Computes prediction statistics
 def compute_statistics(sets:np.ndarray):
+    '''Computes prediction statistics'''
     tp, tn, fp, fn = np.sum(sets, axis=(1, 2, 3))
     with np.errstate(divide='ignore', invalid='ignore'): # Returns Inf when dividing by 0
         recall    = np.divide(tp, (tp + fn)) # Among the building pixels, {recall}% are classified as building
@@ -56,58 +54,68 @@ def compute_statistics(sets:np.ndarray):
     statistics = dict(tp=tp, tn=tn, fp=fp, fn=fn, recall=recall, precision=precision, accuracy=accuracy)
     return statistics
 
-# Displays prediction masks
 def display_statistics(image:np.ndarray, sets:np.ndarray, colour=(255, 255, 0)) -> None:
-        # Formats titles
-        counts = np.sum(sets, axis=(1, 2, 3))
-        titles = ['True positive ({:d})', 'True negative ({:d})', 'False positive ({:d})', 'False negative ({:d})']
-        titles = list(map(lambda title, count: title.format(count), titles, counts))
-        # Formats images
-        images = [np.where(np.tile(mask, (1, 1, 3)), colour, image) for mask in sets]
-        # Displays images
-        fig, axs = pyplot.subplots(2, 2, figsize=(10, 10))
-        for image, title, ax in zip(images, titles, axs.ravel()):
-            ax.imshow(image)
-            ax.set_title(title, fontsize=20)
-            ax.axis('off')
-        pyplot.tight_layout(pad=2.0)
-        pyplot.show()
+    '''Displays prediction masks'''
+    counts = np.sum(sets, axis=(1, 2, 3))
+    titles = ['True positive ({:d})', 'True negative ({:d})', 'False positive ({:d})', 'False negative ({:d})']
+    titles = list(map(lambda title, count: title.format(count), titles, counts))
+    images = [np.where(np.tile(mask, (1, 1, 3)), colour, image) for mask in sets]
+    fig, axs = pyplot.subplots(2, 2, figsize=(10, 10))
+    for image, title, ax in zip(images, titles, axs.ravel()):
+        ax.imshow(image)
+        ax.set_title(title, fontsize=20)
+        ax.axis('off')
+    pyplot.tight_layout(pad=2.0)
+    pyplot.show()
 
-#%% COMPUTES PREDICTION STATISTICS
-
-# Use sklearn.metrics.classification_report
+#%% FORMATS DATA
 
 # Loads model and test data
 labels_test = np.load(path.join(paths['statistics'], 'labels_test.npy'))
 images_test = np.load(path.join(paths['statistics'], 'images_test.npy'))
 model       = models.load_model(path.join(paths['models'], 'unet64_baseline.h5'))
 
-# Subsets data
-subset = np.sum(labels_test, axis=(1, 2, 3)) > 0
-images_test = images_test[subset]
-labels_test = labels_test[subset]
-del subset
-
 # Predicts test data
 probas_pred = layers.Rescaling(1./255)(images_test)
-probas_pred = model.predict(probas_pred)
+probas_pred = model.predict(probas_pred, verbose=1)
 labels_pred = probas_pred >= 0.5
 
+# Subsets data
+subset = np.logical_and(
+    np.sum(labels_test, axis=(1, 2, 3)) > 0,
+    np.sum(labels_pred, axis=(1, 2, 3)) > 0
+)
+images_test = images_test[subset]
+labels_test = labels_test[subset]
+probas_pred = probas_pred[subset]
+labels_pred = labels_pred[subset]
+del subset
+
+#%% COMPUTES PREDICTION STATISTICS
+
 # Compute sets and removes border
-pixsets = np.array(list(map(compute_sets, labels_test, labels_pred)))
-pixsets = np.array(list(map(mask_borders, pixsets, labels_test)))
+sets = np.array(list(map(compute_sets, labels_test, labels_pred)))
+sets = np.array(list(map(mask_borders, sets, labels_test)))
 
 # Aggregated statistics
-stats = np.sum(pixsets, axis=0)
+stats = np.sum(sets, axis=0)
 stats = compute_statistics(stats)
 
 # Statistics per tile
-stats = list(map(compute_statistics, pixsets))
+stats = list(map(compute_statistics, sets))
 stats = DataFrame.from_dict(stats)
 
-# Display statistics
-stats.hist(['precision', 'recall'], bins=25)
-subset = stats.sort_values(by='fn', ascending=False).index[:10]
-for image, pixset in zip(images_test[subset], pixsets[subset]):
-    display_statistics(image, pixset)
+#%% DISPLAYS PREDICTION STATISTICS
+
+# Displays statistics distribution
+fig = pyplot.figure(figsize=(10,5))
+stats.hist(['precision', 'recall'], bins=100, ax=fig.gca())
+del fig
+
+# Displays image statistics
+subset = stats.sort_values(by='fp', ascending=False).index[:10]
+for image, set in zip(images_test[subset], sets[subset]):
+    display_statistics(image, set)
 del subset
+
+# %%
